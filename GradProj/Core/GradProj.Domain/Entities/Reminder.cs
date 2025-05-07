54c5799482b2d8254a19228ec11971482c46b6e7
Kadircan kahveci <kahvecik19@itu.edu.tr>
2025-05-07 17:24:31 +0300

    Adding Tracked and related classes for db and modiyfing Reminder class!

---
+++ GradProj/Core/GradProj.Domain/Entities/Reminder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradProj.Domain.Entities
+{
+    public class Reminder
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+
+        [EnumDataType(typeof(ReferenceType))]
+        public ReferenceType referenceType { get; set; } // The type of the related entity (e.g., Task, Event) 
+
+        public int ReferenceID { get; set; } // The ID of the related task or event 
+        public DateTime ReminderTime { get; set; }
+        public string Message { get; set; } = string.Empty;
+
+        //[ForeignKey(nameof(ReferenceID))]
+        //...Burada hem task olabilir hem event, nasil FK tanimlayacagiz?
+
+        [ForeignKey(nameof(UserID))]
+        public virtual User? User { get; set; }
+
+
+
+    }
+    public enum ReferenceType  
+    {
+        Task,
+        Event
+    }
+    // This enum does not belong here cause
+    // UserEvents or Task Events shoul be able to reach that enum
+    // for setting Reminder
+
+}
